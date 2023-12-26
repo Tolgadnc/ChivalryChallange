4b2602fee1f5d128ce8cd96081a4501809bda93c
Lord Lee <hemanthofficial07@gmail.com>
2023-12-26 18:46:30 +0100

    Third Stage

---
+++ Third_Stage/HealthPotion.java
@@ -0,0 +1,26 @@
+
+
+	public class HealthPotion extends Potion{
+		private int staminaIndex;
+		private int healingPower;
+
+
+//const
+public HealthPotion(String name, String description, int staminaIndex, int healingPower) {
+	super(name, description, staminaIndex, healingPower);
+
+}
+	
+	public int getHealingPower() {
+		return healingPower;
+	}
+
+	
+	public int getStaminaIndex() {
+	  return staminaIndex;
+	}
+
+	public String toString(){
+			return getName();
+		}
+}
